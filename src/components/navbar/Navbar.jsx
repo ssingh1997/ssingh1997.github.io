86c55e424bfe94e39471fccdad1fa4029b8b7887
Satyen Singh <srs833@nyu.edu>
2021-12-01 02:18:37 -0500

    added github icon

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,26 @@
 import "./navbar.scss"
-import {Person, Mail} from "@material-ui/icons"
+import {Person, Mail, GitHub} from "@material-ui/icons"
+import { makeStyles } from '@material-ui/core/styles';
 
 export default function Navbar({ menuOpen, setMenuOpen }) {
+    const useStyles = makeStyles(theme => ({
+        clickableIcon: {
+          cursor: 'default',
+          '&:hover': {
+          cursor: 'pointer',
+          },
+        },
+      }));
+    const classes = useStyles();
     return (
         <div className={"navbar " + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">Satyen.</a>
+                    <div className="itemContainer">
+                        <GitHub onClick={() => window.open("https://github.com/ssingh1997/", "_blank")}
+                        className={classes.clickableIcon}/>
+                    </div>
                     <div className="itemContainer">
                         <Person classname="icon" />
                         <span>+1 973 830 7654</span>
